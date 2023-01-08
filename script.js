4d43cf148049229abfd9861873538e87a2e34255
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2023-01-08 15:15:24 +0100

    import svih imenovanih (named) imports istovremeno

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// IMPORTING MODULE
-import { dodajUKorpu, konacnaCena as cena, konKol } from './shoppingCart.js';
-
+// // IMPORTING MODULE
+// import { dodajUKorpu, konacnaCena as cena, konKol } from './shoppingCart.js';
 console.log('IMPORTING MODULE');
-dodajUKorpu('hleb', 5);
-console.log(cena, konKol);
+// dodajUKorpu('hleb', 5);
+// console.log(cena, konKol);
+
+import * as Korpa from './shoppingCart.js';
+Korpa.dodajUKorpu('majonez', 10);
