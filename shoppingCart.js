ae6e4d70b7a530403aaa35b601765c98e6286b3e
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2023-01-08 14:57:23 +0100

    pravljenje export varijable

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,2 +1,10 @@
 // EXPORTING MODULE
 console.log('EXPORTING MODULE');
+
+const cenaSipinga = 10;
+const korpa = [];
+
+export const dodajUKorpu = function (proizvod, kolicina) {
+  korpa.push({ proizvod, kolicina });
+  console.log(`${kolicina} ${proizvod} je dodat u korpu`);
+};
