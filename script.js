11d7123d860e3e3b9dc326ac6bafad871ab20250
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2023-01-08 19:48:47 +0100

    prikazivanje sadrzaja iz objekta uz pomoc promise-a

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ console.log('IMPORTING MODULE');
 import dodaj from './shoppingCart.js';
 dodaj('pica', 2);
 
-const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-const data = await response.json();
-console.log(data);
-console.log('Nesto');
+// console.log('Pocetak fetchinga');
+// const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await response.json();
+// console.log(data);
+// console.log('Nesto');
+
+const getLastPost = async function () {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// Not very clean nacin da izvucemo sadrzaj iz promise-a
+const lastPost = getLastPost();
+lastPost.then(last => console.log(last));
+
+// Bolji nacin
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
